6c9ec2a8a5fcbded845f6e8636bb29e45db4a1fc
qinfei <qinfei@pwrd.com>
2020-09-22 16:00:29 +0800

    vp2

--- app/src/main/java/com/web/moudle/home/mainFragment/MainFragment.kt
+++ app/src/main/java/com/web/moudle/home/mainFragment/MainFragment.kt
@@ -1,19 +1,18 @@
 package com.web.moudle.home.mainFragment
 
 import android.app.Activity
-import android.content.Intent
 import android.view.View
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 import com.web.common.base.BaseFragment
-import com.web.common.base.BaseFragmentPagerAdapter
-import com.web.common.base.log
 import com.web.moudle.home.HomePageActivity
-import com.web.moudle.home.mainFragment.subFragment.AllArtistFragment
 import com.web.moudle.home.mainFragment.model.MainFragmentViewModel
+import com.web.moudle.home.mainFragment.subFragment.AllArtistFragment
 import com.web.moudle.home.mainFragment.subFragment.BillBoardFragment
 import com.web.moudle.home.mainFragment.subFragment.MusicMainFragment
 import com.web.moudle.home.mainFragment.subFragment.SongSheetFragment
-import com.web.moudle.musicSearch.ui.InternetMusicActivity
 import com.web.moudle.search.SearchActivity
 import com.web.web.R
 import kotlinx.android.synthetic.main.fragment_main.view.*
@@ -21,9 +20,11 @@ import kotlinx.android.synthetic.main.fragment_main.view.*
 class MainFragment: BaseFragment() {
     private lateinit var vm: MainFragmentViewModel
     override fun getLayoutId(): Int = R.layout.fragment_main
+    @Deprecated("重复了")
     private val pageList=ArrayList<BaseFragment>()
 
     init {
+
         pageList.add(MusicMainFragment())
         pageList.add(BillBoardFragment())
         pageList.add(SongSheetFragment())
@@ -33,17 +34,24 @@ class MainFragment: BaseFragment() {
     override fun initView(rootView: View) {
 
         vm= ViewModelProviders.of(this)[MainFragmentViewModel::class.java]
+        rootView.viewPager.offscreenPageLimit = 4
+        rootView.viewPager.adapter= object : FragmentStateAdapter(childFragmentManager,this.lifecycle){
+            override fun getItemCount(): Int = 4
 
-        rootView.viewPager.adapter=BaseFragmentPagerAdapter(fragmentManager!!,pageList)
-        rootView.tabLayout.setupWithViewPager(rootView.viewPager)
-        for(i in pageList.indices){
-            rootView.tabLayout.getTabAt(i)?.text=pageList[i].title
+            override fun createFragment(position: Int): Fragment {
+                return when(position){
+                    0-> MusicMainFragment()
+                    1-> BillBoardFragment()
+                    2-> SongSheetFragment()
+                    else -> AllArtistFragment()
+                }
+            }
         }
+        TabLayoutMediator(rootView.tabLayout,rootView.viewPager) { tab, position -> tab.text = pageList[position].title }.attach()
 
-        rootView.topBar.setEndImageListener(View.OnClickListener {
+        rootView.topBar.setEndImageListener {
             SearchActivity.actionStart(context as Activity, HomePageActivity.searchCode)
-        })
-
+        }
 
 
     }
