44018843b4cd8e5ec905a2f2f1f43d12ba534314
qinfei <qinfei@ezoodb.com>
2025-07-21 15:43:58 +0800

    自定义进度条

--- app/src/main/java/com/compose/common/PlayerInfoComponent.kt
+++ app/src/main/java/com/compose/common/PlayerInfoComponent.kt
@@ -3,36 +3,57 @@ package com.compose.common
 import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderColors
+import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.compose.page.ComposeMusicViewModel
 import com.compose.preview.PreviewMusic
 import com.compose.util.LocalNavi
 import com.compose.util.Navi
+import com.google.android.material.slider.Slider
 import com.music.m.R
 import com.web.common.util.ResUtil
 import com.web.data.Music
 import com.web.moudle.music.player.other.PlayerConfig
 
+@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("ViewModelConstructorInComposable")
 @Preview(showBackground = true)
 @Composable
@@ -42,9 +63,12 @@ fun PlayerInfoComponent(
 ) {
     val vm: ComposeMusicViewModel? = if (!LocalInspectionMode.current) viewModel() else null
     val controller = LocalNavi.current
-    Column(modifier = Modifier.padding(10.dp).clickable{
-        controller.navigate(Navi.MUSIC_DETAIL)
-    }) {
+    Column(
+        modifier = Modifier
+            .padding(10.dp)
+            .clickable {
+                controller.navigate(Navi.MUSIC_DETAIL)
+            }) {
         Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.fillMaxWidth()) {
             val progress = vm?.progress?.collectAsStateWithLifecycle()
             Text(
@@ -53,15 +77,11 @@ fun PlayerInfoComponent(
                     ((music?.duration ?: 0) * (progress?.value ?: 0f)).toLong()
                 )
             )
-            Slider(
-                value = progress?.value ?: 0f,
-                onValueChange = {
-                    vm?.action(ComposeMusicViewModel.Action.Seek(it))
-                },
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(horizontal = 10.dp)
-            )
+
+            CustomSlider(value = progress?.value ?: 0f, modifier = Modifier.weight(1f)
+                .height(10.dp).padding(horizontal = 10.dp)) {
+                vm?.action(ComposeMusicViewModel.Action.Seek(it))
+            }
             Text(text = ResUtil.timeFormat("mm:ss", (music?.duration ?: 0).toLong()))
         }
         Row(
@@ -69,6 +89,7 @@ fun PlayerInfoComponent(
             verticalAlignment = Alignment.CenterVertically
         ) {
             Image(
+                modifier = Modifier.size(60.dp),
                 bitmap = (bitmap
                     ?: ResUtil.getBitmapFromResoucs(
                         R.drawable.singer_default_icon,
---
+++ app/src/main/java/com/compose/common/SliderConponent.kt
@@ -0,0 +1,85 @@
+package com.compose.common
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeightIn
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.compose.page.ComposeMusicViewModel
+import kotlin.math.min
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun CustomSlider(modifier: Modifier = Modifier, value: Float = 0.5f, onValueChanged:((Float) -> Unit) = {}) {
+    val colors = SliderDefaults.colors()
+    var current by remember { mutableFloatStateOf(value) }
+    var isDragging by remember { mutableStateOf(false) }
+    if (!isDragging) {
+        current = value
+    }
+    Slider(
+        value = current,
+        onValueChange = {
+            isDragging = true
+            current = it
+        },
+        onValueChangeFinished = {
+            onValueChanged(current)
+            isDragging = false
+        },
+        modifier = modifier.height(10.dp),
+        thumb = { ss ->
+            Box(modifier = Modifier.fillMaxHeight()) {
+                Canvas(modifier = Modifier
+                    .size(10.dp)
+                    .align(Alignment.Center)) {
+                    drawCircle(colors.thumbColor, radius = min(size.width, size.height) / 2)
+                }
+            }
+        },
+        track = { ss ->
+            Box(modifier = Modifier.height(1.dp)) {
+                Canvas(modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()) {
+                    drawRect(colors.inactiveTrackColor,
+                        topLeft = Offset(ss.value * size.width, 0f),
+                        size = Size(size.width - ss.value * size.width, size.height))
+                    drawRect(colors.activeTrackColor, size = Size(ss.value * size.width, size.height))
+                }
+            }
+
+        }
+    )
+}
